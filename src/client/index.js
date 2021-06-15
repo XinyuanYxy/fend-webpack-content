161ee9021b140b5fd531bd72abdb14ffb802ea8b
Yxychris <yxychris@outlook.com>
2021-06-15 18:32:08 -0400

    added file-loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,4 @@
-alert('hey');
+import { handleSubmit } from './js/formHandler';
+import { checkForName } from './js/nameChecker';
+import { img } from './images/first.png';
+console.log(checkForName);
