e798e9712849bac4fd9595d1d6e51751fd3797be
Yxychris <yxychris@outlook.com>
2021-06-16 17:16:17 -0400

    added plugin

--- src/server/index.js
+++ src/server/index.js
@@ -4,14 +4,13 @@ const mockAPIResponse = require('./mockAPI.js');
 
 const app = express();
 
-app.use(express.static('src/client'));
+app.use(express.static('dist'));
 
 console.log(__dirname);
 
 app.get('/', function (req, res) {
-	res.sendFile('/client/views/index.html', { root: __dirname + '/..' });
+	res.sendFile('dist/index.html', { root: '../../' });
 });
-
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
 	console.log('Example app listening on port 8080!');
